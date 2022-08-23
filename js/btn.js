a460170eaafe6ae2b6c89e63b15f8d33ad3f8327
hasanshanto01 <hasanshanto01@gmail.com>
2022-08-24 05:57:06 +0600

    function implementation

--- js/btn.js
+++ js/btn.js
@@ -1,88 +1,72 @@
-// console.log('connected');
 let playerNameList = [];
-// let playerNmuber = playerNameList.length;
 
-// add player to select field
+// get element field by id
+function getElementFieldById(elementFieldId) {
+    const elementField = document.getElementById(elementFieldId);
+    return elementField;
+}
 
-// player total function
-// function calculatePlayerTotal() {
+// alert message
+function alertMessage(id) {
+    alert('Enter number!!!')
+    getElementFieldById(id).innerText = '00';
+}
 
-// }
-
-
-//player add for click select
+//player add to select-v on click select button
 document.getElementById('player-card').addEventListener('click', function (event) {
     const targetElement = event.target;
     const targetElementText = targetElement.innerText;
-    // const disablitiy = targetElement.parentNode;
-    // console.log(targetElement)
-
-    // let playerName = '';
 
     if (targetElementText.toLowerCase() === 'select') {
         const playerName = targetElement.parentNode.children[0].innerText;
-        // console.log(playerName)
-
         playerNameList.push(playerName);
 
-        let selectPlayerField = document.getElementById('select-player-field');
+        let selectPlayerField = getElementFieldById('select-player-field');
         const li = document.createElement('li');
         li.innerText = playerName;
 
         if (playerNameList.length <= 5) {
             selectPlayerField.appendChild(li);
             targetElement.disabled = true;
-            // console.log(selectPlayerField, playerNameList.length)
         }
         else {
-            alert('You already select five players!')
-            playerNameList.pop()
-            // console.log(playerNameList.length)
+            alert('You already select five players!');
+            playerNameList.pop();
         }
 
     }
-    else {
-        console.log('other')
-    }
-
-
 })
 
 
 // calculate player total
-
 document.getElementById('calculate-player-total').addEventListener('click', function () {
-    const perPlayerPrice = parseInt(document.getElementById('per-player-price').value);
+    const perPlayerPrice = parseInt(getElementFieldById('per-player-price').value);
+
     if (isNaN(perPlayerPrice) != true) {
         const playerTotal = perPlayerPrice * playerNameList.length;
 
-        const playerExpenses = parseInt(document.getElementById('player-expenses').innerText);
-        document.getElementById('player-expenses').innerText = playerTotal;
+        const playerExpenses = parseInt(getElementFieldById('player-expenses').innerText);
+        getElementFieldById('player-expenses').innerText = playerTotal;
     }
     else {
-        alert('Enter number!')
-        document.getElementById('player-expenses').innerText = '00';
+        alertMessage('player-expenses');
     }
-    // console.log(playerTotal, typeof playerExpenses, typeof perPlayerPrice)
 })
 
 // calculate total
 document.getElementById('calculate-total').addEventListener('click', function () {
-    // console.log('calculate total clicked') 
-    const managerPrice = parseInt(document.getElementById('manager-price').value);
-    const coachPrice = parseInt(document.getElementById('coach-price').value);
-    let total = parseInt(document.getElementById('total').innerText);
-    // console.log(total)
+    const managerPrice = parseInt(getElementFieldById('manager-price').value);
+    const coachPrice = parseInt(getElementFieldById('coach-price').value);
+    let total = parseInt(getElementFieldById('total').innerText);
+
     if (isNaN(managerPrice) != true && isNaN(coachPrice) != true) {
-        const playerExpenses = parseInt(document.getElementById('player-expenses').innerText);
+        const playerExpenses = parseInt(getElementFieldById('player-expenses').innerText);
 
         total = playerExpenses + managerPrice + coachPrice;
-        document.getElementById('total').innerText = total;
-        console.log(total)
+        getElementFieldById('total').innerText = total;
     }
     else {
-        alert('Enter number!')
-        document.getElementById('total').innerText = '00';
+        alertMessage('total');
     }
-    // console.log(isNaN(managerPrice), typeof coachPrice)
+
 })
